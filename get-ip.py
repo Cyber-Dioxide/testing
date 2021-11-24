a06668707f5268cbebe18d2d5782840b0e115324
Saadkhan041 <93708296+Saadkhan041@users.noreply.github.com>
2021-11-24 10:07:38 +0500

    Update get-ip.py

--- get-ip.py
+++ get-ip.py
@@ -1,13 +1,31 @@
 from os import system as sys
 from urllib.request import urlopen
+import random
 from urllib.request import Request
 from bs4 import BeautifulSoup as BS
+black='\033[30m'
+red='\033[31m'
+green='\033[32m'
+orange='\033[33m'
+blue='\033[34m'
+purple='\033[35m'
+cyan='\033[36m'
+lightgrey='\033[37m'
+darkgrey='\033[90m'
+lightred='\033[91m'
+lightgreen='\033[92m'
+yellow='\033[93m'
+lightblue='\033[94m'
+pink='\033[95m'
+lightcyan='\033[96m'
+all_col = [red,green,orange,cyan,lightgrey,lightred,lightgreen,yellow,lightcyan]
+ran = random.choice(all_col)
+while True:
 
-sys('clear')
-
-class ip:
-    def banner(self):
-        print ('''
+    sys('clear')
+    class ip:
+        def banner(self):
+            p ='''
    ________        __            .___        
   /  _____/  _____/  |_          |   |_____  
  /   \  ____/ __ \   __\  ______ |   \____ \ 
@@ -15,23 +33,34 @@ class ip:
   \______  /\___  >__|           |___|   __/ 
          \/     \/                   |__|    
 ______________________________________________
-''')
+'''
+
+    print(ran , p)
     def public_ip(self):
         headers = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0"}
         url = Request('https://www.myip.com/',headers=headers)
         openurl = urlopen(url).read(99999)
         soup = BS(openurl , 'html.parser')
         for i in soup.findAll('span',{'id':'ip'}):
-            print (f' [+] Public Ip > {i.text}')
+            print (f'{ran} [+] Public Ip > {i.text}')
 
     def privet_ip(self):
         sys("printf ' [+] Privet Ip > ';ifconfig wlan0 | grep 'netmask' | awk -F ' ' '{print $2}'")
 
     def localhost(self):
-        print(" [+] LocalHost > 127.0.0.1\n")
+        print(ran," [+] LocalHost > 127.0.0.1\n")
 
-ip = ip()
-ip.banner()
-ip.public_ip()
-ip.privet_ip()
-ip.localhost()
+    ip = ip()
+    ip.banner()
+    ip.public_ip()
+    ip.privet_ip()
+    ip.localhost()
+
+    cont = input(f"{cyan}[-]Do You want to continue? [y/n]: ").lower
+
+    if cont == "n" or "no":
+        break
+    else:
+        pass
+
+
