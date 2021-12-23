312d0d0fd26c0e2e0be6c06a6e7c30d57ae44828
Saad <93708296+Saadkhan041@users.noreply.github.com>
2021-12-23 12:15:34 +0500

    Add files via upload

--- guess.py
+++ guess.py
@@ -7,29 +7,29 @@ lbiu = MyColors.lightColorsBIU()
 nbiu = MyColors.NormalBIUColors()
 
 nbold = MyColors.NormalBoldColors()
-lbold = MyColors.lightColorsBold()
+nbold = MyColors.lightColorsBold()
 
 def game():
-    name = input(lbold.LIGHT_greenBold("Your name: "))
-    lbold.LIGHT_megantaBold("Welcome")
-    lbold.LIGHT_redBold(pyfiglet.figlet_format(name))
+    name = input(nbold.LIGHT_greenBold("Your name: "))
+    nbold.LIGHT_greenBold("Welcome")
+    nbold.LIGHT_cyanBold(pyfiglet.figlet_format(name))
     rand = random.randint(1 , 100)
     ranran = random.randrange(5 ,100 ,5)
     l = rand + rand
     lbiu.LIGHT_redBIU("\nThink a number")
-    input(nbold.cyanBold("\nHit enter to continue! :"))
+    input(nbold.LIGHT_cyanBold("\nHit enter to continue! :"))
     p = l +ranran
-    nbold.cyanBold(f"Add {ranran} in the number you've thought! ")
-    input(lbold.LIGHT_megantaBold("Hit enter to continue! :"))
+    nbold.LIGHT_cyanBold(f"Add {ranran} in the number you've thought! ")
+    input(nbold.LIGHT_megantaBold("Hit enter to continue! :"))
     p = p/2
-    nbold.cyanBold(f"Divide the total bu 2! :")
-    input(nbold.cyanBold("\nHit enter to continue! :"))
+    nbold.LIGHT_cyanBold(f"Divide the total bu 2! :")
+    input(nbold.LIGHT_cyanBold("\nHit enter to continue! :"))
 
     p = p-rand
-    lbold.LIGHT_greenBold(f"Sub {p} from lefted amount! ")
-    input(nbold.cyanBold("\nHit enter to continue! :"))
+    nbold.LIGHT_greenBold(f"Sub {p} from lefted amount! ")
+    input(nbold.LIGHT_cyanBold("\nHit enter to continue! :"))
 
-    lbold.LIGHT_yellowBold(f"You are left with {p}")
+    nbold.LIGHT_yellowBold(f"You are left with {p}")
 
 no = ["n", "no"]
 cont = ""
@@ -37,7 +37,7 @@ cont = ""
 while cont not in no:
     game()
 
-    cont = input(nbold.cyanBold("Do you want to play again!"))
+    cont = input(nbold.LIGHT_cyanBold("Do you want to play again!"))
 
 
 
