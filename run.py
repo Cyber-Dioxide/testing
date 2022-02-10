45ed4489d8a3670833747f17ca00a028d75b15d2
Saad <93708296+Saadkhan041@users.noreply.github.com>
2022-02-10 19:20:16 +0500

    Add files via upload

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from platform import platform
 def cmd(str):
     os.system(str)
 
-# cmd("apt install ruby")
+
 
 os.system("cls") if "Windows" in platform() else os.system("clear")
 
@@ -21,25 +21,9 @@ C = Fore.CYAN
 Y = Fore.YELLOW
 
 
-if sys.argv[0].isalpha():
-    print(W +"Usage: python3 run.py [range of ip]")
-    print(C +"Note: range of ip should be numeric.")
-    exit()
+rang = sys.argv[0]
+cmd("apt install ruby")
 
-if sys.argv[0] == "":
-    print(R +"Usage: python3 run.py [range of ip]")
-    print(G +"Note: range of ip should be numeric.")
-    exit()
-
-if sys.argv[0].isalnum():
-    print(R + "Usage: python3 run.py [range of ip]")
-    print(G + "Note: range of ip should be numeric.")
-    exit()
-
-
-if sys.argv[0].isdigit():
-    rang = sys.argv[0]
-
-    cmd(f"ruby Gen.rb {rang}")
+cmd(f"ruby Gen.rb {rang}")
 
 
